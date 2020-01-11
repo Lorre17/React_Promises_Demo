e6f753db137b98b1d2f12b758aeca4d2c6bab406
Lorena Strechie <lorena.strechie@yahoo.com>
2020-01-11 18:23:02 +0000

    Added Error Handling with Promise.prototype.catch()

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const output = document.getElementById('output')
 output.innerText = 'Loading...'
 
 fetch(API_URL + 'films')
-  .then(response => response.json())
-  .then(films => {
-    output.innerText = getFilmTitles(films)
+  .then(async response => {
+    if(!response.ok) {
+      throw Error("Unsuccessful response");
+    }
+    const films = await response.json()
+    return (output.innerText = getFilmTitles(films))
+  })
+  .catch(error => {
+    console.warn(error)
+    output.innerText = ':('
   })
 
-  function getFilmTitles(films) {
-    return films
+function getFilmTitles(films) {
+  return films
     .sort((a, b) => a.episode_id - b.episode_id)
     .map(film => `${film.episode_id}. ${film.title}`)
     .join('\n')
-  }
+}

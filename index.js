b5e7d93c9f9a25595faa9888ce7bff5939a50672
Lorena Strechie <lorena.strechie@yahoo.com>
2020-01-13 10:46:19 +0000

    Executes cleanup logic wiith Promise.prototype.finally()

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+// #region Setup
 const API_URL = 'https://starwars.egghead.training/'
 const output = document.getElementById('output')
+const spinner = document.getElementById('spinner')
 
-output.innerText = 'Loading...'
+function getFilmTitles(films) {
+  return films
+    .sort((a, b) => a.episode_id - b.episode_id)
+    .map(film => `${film.episode_id}. ${film.title}`)
+    .join('\n')
+}
+// #region End
 
 fetch(API_URL + 'films')
   .then(async response => {
-    if(!response.ok) {
-      throw Error("Unsuccessful response");
+    if (!response.ok) {
+      throw Error('Unsuccessful response')
     }
     const films = await response.json()
     return (output.innerText = getFilmTitles(films))
@@ -15,10 +23,6 @@ fetch(API_URL + 'films')
     console.warn(error)
     output.innerText = ':('
   })
-
-function getFilmTitles(films) {
-  return films
-    .sort((a, b) => a.episode_id - b.episode_id)
-    .map(film => `${film.episode_id}. ${film.title}`)
-    .join('\n')
-}
+  .finally(() => {
+    spinner.remove()
+  })
